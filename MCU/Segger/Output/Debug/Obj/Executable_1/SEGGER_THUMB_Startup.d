674288d9c2143758e9ef9ed1860503d13e433579
Jackson <jacksonphilion@gmail.com>
2024-10-10 18:09:10 -0700

    Organize

--- MCU/Segger/Output/Debug/Obj/Executable_1/SEGGER_THUMB_Startup.d
+++
@@ -1,2 +0,0 @@
-Output/Debug/Obj/Executable_1/SEGGER_THUMB_Startup.o: \
- C:\Users\jphilion\Desktop\e155_lab4\MCU\Segger\SEGGER_THUMB_Startup.s
--- MCU/Segger/Output/Debug/Obj/Executable_1/stm32l432xx_Vectors.d
+++
@@ -1,2 +0,0 @@
-Output/Debug/Obj/Executable_1/stm32l432xx_Vectors.o: \
- C:\Users\jphilion\Desktop\e155_lab4\MCU\Segger\STM32L4xx\Source\stm32l432xx_Vectors.s
